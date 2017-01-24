0de597578d658923f6fb82127744462dd7dfbe88
Lukas Urban <lukasurban@online.de>
2017-01-24 12:16:56 +0100

    added method calcSize

--- U11_LukasUrban/src/hospital/heap/ArrayListHeap.java
+++ U11_LukasUrban/src/hospital/heap/ArrayListHeap.java
@@ -4,16 +4,26 @@ public class ArrayListHeap extends AbstractBinaryHeap {
 	
 	@Override
 	public int getSize() {
-		//if root != null calcSize(root) | else return 0
-		//if getRightChildren != null +2
-			//else if getLeftChildren != +1
-				//else if there is marker calcSize(stageMarker)
-					//else return val
-		//set stageMarker on rightChild
-		//calcSize(leftChild)
-		return 0;
+		
+		if (top() != null) return calcSize(top(), 0); 
+		else return 0;
+
+	}
+	
+	
+	public int calcSize(T knot,int count) {
+/*		
+*		if getRightChildren != null count += 2
+*		else if getLeftChildren != count++
+*			else if there is marker calcSize(stageMarker)
+*				else return val
+*	set stageMarker on rightChild
+*	calcSize(leftChild)
+*/
+		return count;
 	}
 
+	
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
